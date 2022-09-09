bbdcbb0b2b2bb2009c44c2606f4ba8d5da3efb2c
Johan Apolinario Véliz <johelapo@gmail.com>
2022-09-09 11:47:43 -0500

    Darkmode is now working

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react';
-import './index.css';
-import TextUtils from './components/TextUtils';
-
-
+import { useState } from "react";
+import "./index.css";
+import TextUtils from "./components/TextUtils";
 
 const App = () => {
-    const [darkMode, setDarkMode] = useState(false);
-    if (!darkMode) {
-        document.body.style.backgroundColor = 'black'
-        document.body.style.color = 'white'
-    } else if (darkMode) {
-        document.body.style.backgroundColor = 'white'
-        document.body.style.color = 'black'
-    }
-    
-    return (
-        <>
+  const [isDarkModeEnabled, setIsDarkModeEnabled] = useState(false);
+  if (!isDarkModeEnabled) {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  } else if (isDarkModeEnabled) {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  }
 
-            <div className="d-flex flex-row justify-content-end"><button onClick={darkMode ? setDarkMode(true): setDarkMode(false)} className='btn btn-outline-dark'>Enable {!darkMode ? "light" : "dark"} Mode</button></div>
-            <div className="container-fluid">
-                <TextUtils heading="TextUtils" />
-            </div>
+  function handleClick() {
+    isDarkModeEnabled
+      ? setIsDarkModeEnabled(false)
+      : setIsDarkModeEnabled(true);
+  }
 
-        </>
-    )
-}
+  return (
+    <>
+      <header className="d-flex flex-row justify-content-end p-5">
+        <button
+          onClick={handleClick}
+          className={`btn btn-outline-${isDarkModeEnabled ? "dark" : "light"}`}
+        >
+          Enable {!isDarkModeEnabled ? "light" : "dark"} Mode
+        </button>
+      </header>
+      <TextUtils heading="TextUtils" />
+    </>
+  );
+};
 
 export default App;
